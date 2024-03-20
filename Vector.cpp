05b01c6db3e2d3ea4e4646ba92bce89492022553
Priesthoo <163196772+Priesthoo@users.noreply.github.com>
2024-03-20 15:15:21 +0100

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,5 +1,4 @@
 #include<iostream>
-#include<string>
 #include<initializer_list>
 using namespace std;
 template<class T>
@@ -27,7 +26,24 @@ class Vector{
         }
         return y;
     }
-    
+ static int get_length_from_const_pos(const_iterator iter1,const_iterator iter2){
+     int y=0;
+     iterator iter3=iter1;
+     while(iter3!=iter2){
+         ++iter1;
+         ++y;
+     }
+     return y;
+ }
+ void print_out(){
+     for(int i=0;i<size;i++){
+         cout<<str[i]<<endl;
+     }
+ }
+  explicit Vector(const size_t& value):size{value}
+  {
+      str=new T[size];
+  }
   Vector(initializer_list<T> val){
       size=val.size();
       str=new T[size];
@@ -204,5 +220,153 @@ class Vector{
       iterator iter1=(*this).begin()+f;
       return iter1;
   }
-    
-};
+  iterator insert(iterator pos, initializer_list<T> val ){
+      Vector<T> vec=*this;
+      auto iter1=val.begin();
+      auto iter2=val.end();
+      int y=0;
+        while(iter1!=iter2){
+            ++iter1;
+            ++y;
+        }
+       auto iter4=val.begin();
+      int f=Vector<T>::get_length_from_pos((*this).begin(),pos);
+      int len1=size+y;
+      size=len1;
+      str=new T[size];
+      for(int i=0;i<f;i++){
+          str[i]=vec.str[i];
+      }
+      for(int i=0;i<y;i++){
+          str[f+i]=*(iter4+i);
+      }
+      int len2=f+y;
+      int len3=size-len2;
+      for(int i=0;i<len3;i++){
+          str[len2+i]=vec.str[f+i];
+      }
+      iterator iter3=(*this).begin()+f;
+      return iter3;
+  }
+void insert_at_pos(iterator pos,const T& elem){
+  Vector<T> vec=*this;
+  int y=Vector<T>::get_length_from_pos((*this).begin(),pos);
+  str=new T[size];
+  for(int i=0;i<y;i++){
+      str[i]=vec.str[i];
+  }
+  for(int i=y;i<size;i++){
+      if(i==y){
+          str[i]=elem;
+      }
+      else{
+          str[i]=vec.str[i];
+      }
+  }
+  }
+  iterator erase(iterator pos)  {
+      iterator iter=(*this).begin();
+      Vector<T> vec=*this;
+      int y=Vector<T>::get_length_from_pos(iter,pos);
+      for(int i=0;i<y;i++){
+          str[i]=vec.str[i];
+      }
+      for(int i=y;i<vec.get_length();i++){
+          str[i]=vec.str[i+1];
+      }
+      Vector<T> vec1=*this;
+      --size;
+      str=new T[size];
+      for(int i=0;i<size;i++){
+          str[i]=vec1.str[i];
+      }
+      iterator iter1=(*this).begin()+y;
+      return iter1;
+      }  
+      iterator erase(iterator beg,iterator end) {
+      Vector<T> vec=*this;
+      int iter=Vector<T>::get_length_from_pos((*this).begin(),beg);
+      int iter2=Vector<T>::get_length_from_pos(beg,end);
+      int j=iter+iter2;
+      for(int i=0;i<iter2;i++){
+          str[i+iter]=0;
+      }
+      
+      for(int i=0;i<iter2;i++){
+          str[i+iter]=vec.str[i+j];
+      }
+      Vector<T> vec1=*this;
+      size=iter2+vec1.get_length()-j-1;
+      str=new T[size];
+      for(int i=0;i<size;i++){
+          str[i]=vec1.str[i];
+      }
+      iterator iter1=(*this).begin()+iter;
+      return iter1;
+      }
+   void pop_front(){
+       Vector<T> vec=*this;
+       for(int i=0;i<size-1;i++){
+           str[i]=vec.str[i+1];
+       }
+       Vector<T> vec1=*this;
+       --size;
+       str=new T[size];
+       for(int i=0;i<size;i++){
+           str[i]=vec1.str[i];
+       }
+   }
+   void resize(const int& num){
+       Vector<T>vec=*this;
+       int newsize=size+num;
+       str=new T[newsize]; //it returns  a pointer to the allocated memory in the heap....
+       for(int i=0;i<size;i++){
+           str[i]=vec.str[i];
+       }
+       for(int i=size;i<newsize;i++){
+           str[i]=0;
+       }
+       size=newsize;
+      }
+      void resize(const int& num,const T& elem){
+       Vector<T>vec=*this;
+       int newsize=size+num;
+       str=new T[newsize]; //it returns  a pointer to the allocated memory in the heap....
+       for(int i=0;i<size;i++){
+           str[i]=vec.str[i];
+       }
+       for(int i=size;i<newsize;i++){
+           str[i]=elem;
+       }
+       size=newsize;
+      }
+      void resize(const int& num,iterator begin){
+       Vector<T>vec=*this;
+       int newsize=size+num;
+       str=new T[newsize]; //it returns  a pointer to the allocated memory in the heap....
+       for(int i=0;i<size;i++){
+           str[i]=vec.str[i];
+       }
+       for(int i=0;i<newsize-size;i++){
+           str[i+size]=*(begin+i);
+       }
+       size=newsize;
+      }
+      void resize(const int& num,const initializer_list<T>& val){
+       Vector<T>vec=*this;
+       int newsize=size+num;
+       str=new T[newsize]; //it returns a pointer to the allocated memory in the heap....
+       for(int i=0;i<size;i++){
+           str[i]=vec.str[i];
+       }
+       for(int i=0;i<newsize-size;i++){
+           str[i]=*(val.begin()+i);
+       }
+       size=newsize;
+      }
+     void clear(){
+      size=0;
+      str=nullptr;
+      } 
+  };
+
