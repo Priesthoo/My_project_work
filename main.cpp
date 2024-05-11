94ab2c6763fbbad206a26e78f20a2071cb576a44
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-05-11 19:46:02 +0100

    Update main.cpp

---
+++ main.cpp
@@ -0,0 +1,15 @@
+int main(int argc, char *argv[])
+{    int val=67;
+     Bnode<int>*bnode=new Bnode<int>;
+     bnode->value=val;
+     bnode->right=null;
+     bnode->left=null;
+     create_node(bnode,70);
+     insert_new_node(bnode,17);
+     insert_new_node(bnode,18);
+      cout<<bnode->value<<endl;
+     cout<<bnode->right->value<<endl;
+     cout<<bnode->left->right->value;
+     
+     
+}
