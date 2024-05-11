7d5e9b052dfcdd7261afc327acd5eee47648c3a1
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-05-11 19:39:05 +0100

    Update Test.h

--- Test.h
+++ Test.h
@@ -11,6 +11,12 @@ struct Bnode{
     T value;
     Bnode* right;
     Bnode* left;
+    Bnode()=default;
+    Bnode(const T& val,Bnode*r,Bnode*l){
+        value=val;
+        right=r;
+        left=l;
+    }
 };
 /*
 we assume  that bnode is pointing an allocated memory..
@@ -41,3 +47,36 @@ void create_node(Bnode<T>* bnode,const T& val){
         }
     }
  }
+template<class T>
+void insert_new_node(Bnode<T>*bnode,const T& value){
+    if(bnode!=null){
+        if(value<bnode->value){
+            if(bnode->left==null){
+                bnode->left=new Bnode<T>;
+                bnode->left->value=value;
+                bnode->left->right=null;
+                bnode->left->left=null;
+                }
+                else{
+                    insert_new_node(bnode->left,value);
+                }
+        }
+        else if(value>bnode->value){
+            if(bnode->right==null){
+                bnode->right=new Bnode<T>;
+                bnode->right->value=value;
+                bnode->right->right=null;
+                bnode->right->left=null;
+            }
+            else{
+                insert_new_node(bnode->right,value);
+            }
+        }
+    }
+    else{
+        bnode=new Bnode<T>;
+        bnode->value=value;
+        bnode->right=null;
+        bnode->left=null;
+    }
+}
