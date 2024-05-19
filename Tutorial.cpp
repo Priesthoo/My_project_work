6f5bb6283123857c43cb0a74eba6aff8f0b58b3a
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-05-19 23:34:18 +0100

    Update Tutorial.cpp

--- Tutorial.cpp
+++ Tutorial.cpp
@@ -1,11 +1,11 @@
  #include<iostream>
-
+using namespace std;
 //Tutorial session...
-	Circular_list<int>clist={12,13,14,15,16,17,18,19};
+int main(){	Circular_list<int>clist={12,13,14,15,16,17,18,19};
 	print_nl("clist:");
 	clist.print_clist();
 	print_fn("first value of clist:");
 	print_value(clist[0]);
 	print_fn("second value of clist:");
 	print_value(clist[1]);
-	
+}	
