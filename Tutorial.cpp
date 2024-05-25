a48dbf9b4a69dedea91bd36572123a1b3ef13439
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-05-25 02:01:11 +0100

    Update Tutorial.cpp

--- Tutorial.cpp
+++ Tutorial.cpp
@@ -2,18 +2,19 @@
 using namespace std;
 //Tutorial session...
 int main(){	
-
- //Tutorial session 
-print_nl(" values in the cicular list:");
-Circular_list<int>clist={12,13,14,15,16,17,18,19,20};
-clist.print_clist();
-
-Node<int>*iter=clist.get_Nth_node_for_clist(1);
-print_fn("the first element accessed by inserting 1 as an argument:");
-print_value(iter->value);
-cout<<endl;
-print_fn("the second element accessed by inserting 2 as an argument");
-iter=clist.get_Nth_node_for_clist(2);
-print_value(iter->value);
-
-}	
+Node<int>*node=new Node<int>;
+    init(node,{12,14,15,17,18,19,20,21,22,34,56,78,68});
+    Dlist<int> dlist={node};
+    cout<<"values of double linked list:"<<endl;
+    dlist.print_dlist_forward();
+    Node<int>*nNode=dlist.Get_nth_node(5);
+    print(nNode);
+    cout<<endl;
+    cout<<"Inserting 67 after the 5th element:"<<endl;
+    dlist.insert_after_pos(nNode,67);
+    dlist.print_dlist_forward();
+    cout<<endl;
+    cout<<"inserting 59 before the 5th element:"<<endl;
+    dlist.insert_before_pos(nNode,59);
+    dlist.print_dlist_forward();
+}
