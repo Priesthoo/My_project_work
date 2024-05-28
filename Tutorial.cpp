6db0cff55d74506e0918ceae82759cfe31cbb6ad
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-05-28 04:37:34 +0100

    Update Tutorial.cpp

--- Tutorial.cpp
+++ Tutorial.cpp
@@ -1,20 +1,12 @@
- #include<iostream>
+#include<iostream>
+#include"Hash_map.h"
 using namespace std;
 //Tutorial session...
 int main(){	
-Node<int>*node=new Node<int>;
-    init(node,{12,14,15,17,18,19,20,21,22,34,56,78,68});
-    Dlist<int> dlist={node};
-    cout<<"values of double linked list:"<<endl;
-    dlist.print_dlist_forward();
-    Node<int>*nNode=dlist.Get_nth_node(5);
-    print(nNode);
-    cout<<endl;
-    cout<<"Inserting 67 after the 5th element:"<<endl;
-    dlist.insert_after_pos(nNode,67);
-    dlist.print_dlist_forward();
-    cout<<endl;
-    cout<<"inserting 59 before the 5th element:"<<endl;
-    dlist.insert_before_pos(nNode,59);
-    dlist.print_dlist_forward();
+String_map<string,string>smap={{"Hardwork","busy"},{"Hardwork","diligent"},{"Harwdork","industrious"},{"Hardwork","assiduous"},{"Hardwork","energetic"},{"Hardwork","employed"}};
+smap.print_table();
+cout<<endl;
+cout<<"using string map as a synonyms dictionary"<<endl;
+cout<<"words similar to hardwork:"<<endl;
+smap.print_synonyms("Hardwork");
 }
