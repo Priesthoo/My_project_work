c9c91b5a6c59b32298f89e795a5df8eabeae328d
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-05-30 22:23:26 +0100

    Update make_up.h

--- make_up.h
+++ make_up.h
@@ -1,7 +1,14 @@
 #include<iostream>
 #include<string>
 #include<sstream>
+#include<cmath> //for pow(n,t);
+#define number 0x7FFF
 using namespace std;
+enum BASE{
+    BASE_TWO=1<<1,
+    BASE_EIGHT=1<<3,
+    BASE_SIXTEEN=1<<4
+};
 
 string Reverse_string(const string& str){
     string rstr={" ",str.size()};
@@ -22,14 +29,17 @@ char convert(const int& c){
         return '0';
     }
 }
-int convert_to_binary(const int& value,const int& divide){
+long long convert_to_binary(const long long& value,const int& divide){
+    if(value==0){
+        return 0;
+    }
     string str={};
-    int v=value;
+    long long v=value;
     stringstream str1;
     int c=0;
     while(v>=divide){
         c=v%divide;
-        int f=v/divide;
+        long long  f=v/divide;
         char c1=convert(c);
         str.push_back(c1);
         v=f;
@@ -40,4 +50,32 @@ int convert_to_binary(const int& value,const int& divide){
     str1>>v;
     return v;
 }
-i
+int convert_bin(const char c){
+    if(c=='1'){
+        return 1;
+    }
+    else if(c=='0'){
+        return 0;
+    }
+}
+long long convert_to_decimal(const long long& binary,BASE base=BASE_TWO){
+    stringstream str3;
+    str3<<binary;
+    string str1=str3.str();
+    long long  v=0;
+    long long  j=0;
+    string str2=Reverse_string(str1);
+    for(int i=str1.size()-1;i>=0;--i){
+        v=convert_bin(str2[i]);
+        v*=pow(2,i);
+        j+=v;
+    }
+    return j;
+}
+long long convert_hex_to_binary(long long hex){
+    long long  v=convert_to_binary(hex,2);
+    return v;
+}
+void print(const long long& value,const int& v){
+    cout<<value<<"     |    "<<v<<endl;
+}
