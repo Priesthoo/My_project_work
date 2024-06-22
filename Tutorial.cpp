9f87fe08a461ffb2af7571ca591189bda092738a
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-06-22 22:59:36 +0100

    Update Tutorial.cpp

--- Tutorial.cpp
+++ Tutorial.cpp
@@ -4,6 +4,6 @@ using namespace std;
 int main(){
 Bit<8> bits1={56};
 Bit<16>bits2={112};
-Bit<8>bit3=bits1 & bits2;
+Bit<8>bit3=bits1 & bits2;//operator overloading(I overloaded the bitwise operator)..
 Bit<8>::print_bit_pattern(bit3);
 }
