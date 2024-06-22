269a8daef00e63a71531ed5bcca28c24a5a1c2f4
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-06-22 22:58:30 +0100

    Update Tutorial.cpp

--- Tutorial.cpp
+++ Tutorial.cpp
@@ -1,5 +1,9 @@
- Bit<8> bits1={56};
- Bit<16>bits2={112};
- Bit<8>bit3=bits1 & bits2;
- Bit<8>::print_bit_pattern(bit3);
- 
+#include<iostream>
+using namespace std;
+#include"make_up.h"
+int main(){
+Bit<8> bits1={56};
+Bit<16>bits2={112};
+Bit<8>bit3=bits1 & bits2;
+Bit<8>::print_bit_pattern(bit3);
+}
