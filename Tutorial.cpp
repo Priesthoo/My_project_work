120c36ebb8781a2c0caad5f39d629e905d9b7542
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-06-22 22:56:48 +0100

    Update Tutorial.cpp

--- Tutorial.cpp
+++ Tutorial.cpp
@@ -1,12 +1,5 @@
-#include<iostream>
-#include"Hash_map.h"
-using namespace std;
-//Tutorial session...
-int main(){	
-String_map<string,string>smap={{"Hardwork","busy"},{"Hardwork","diligent"},{"Harwdork","industrious"},{"Hardwork","assiduous"},{"Hardwork","energetic"},{"Hardwork","employed"}};
-smap.print_table();
-cout<<endl;
-cout<<"using string map as a synonyms dictionary"<<endl;
-cout<<"words similar to hardwork:"<<endl;
-smap.print_synonyms("Hardwork");
-}
+ Bit<8> bits1={56};
+ Bit<16>bits2={112};
+ Bit<8>bit3=bits1 & bits2;
+ Bit<8>::print_bit_pattern(bit3);
+ 
