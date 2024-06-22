10e22a7827b0d9ba17cb8cae2499c11bc50d4a21
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2024-06-22 23:06:53 +0100

    Update Tutorial.cpp

--- Tutorial.cpp
+++ Tutorial.cpp
@@ -4,6 +4,11 @@ using namespace std;
 int main(){
 Bit<8> bits1={56};
 Bit<16>bits2={112};
-Bit<8>bit3=bits1 & bits2;//operator overloading(I overloaded the bitwise operator)..
-Bit<8>::print_bit_pattern(bit3);
+Bit<8>bit3=bits1 & bits2;//operator overloading(I overloaded the bitwise operator and(&))..
+Bit<8>::print_bit_pattern(bit3);//it will output "Cast to same size, The sizes are not equal"
+
+Bit<16>bit1={567};
+Bit<16>bit2={700};
+Bit<16>bit4= bit1 & bit2;
+Bit<16>::print_bit_pattern(bit4);// it will output the result of bitwise anding bit1 and bit2
 }
