c317da93747eda6b144102613e63c9c93549eba8
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2025-01-16 10:44:38 -0800

    Update Static_Polymorphism.cpp

--- Static_Polymorphism.cpp
+++ Static_Polymorphism.cpp
@@ -1,1 +1,57 @@
+ #include<iostream>
+#include<memory>
+using namespace std;
+// This class will  serve as a base class for all derived classes that will be defined here,It will also implement the function that will be shared across derived class
+template<class T>
+class Strategy{
+    public:
+    void execute_all_strategy(){
+        //execute() is the function that will be shared across the derived class
+        static_cast<T*>(this)->execute();
+    }
+   ~Strategy(){
+       
+   }
+};
+//An inheritor
+class Defensive_Strategy:public Strategy<Defensive_Strategy>{
+   public:
+   void execute(){
+       std::cout<<"Using Defensive Strategy"<<std::endl;
+   }
+~Defensive_Strategy(){
     
+}
+};
+//An inheritor
+class Offensive_Strategy:public Strategy<Offensive_Strategy>{
+    public:
+    void execute(){
+        std::cout<<"Using Offensive Strategy"<<std::endl;
+    }
+    ~Offensive_Strategy(){
+        
+    }
+};
+
+class Player{
+    public:
+template<class T>
+    void change_strategy(){
+        T strategy;
+        strategy.execute_all_strategy();
+       return;
+    }
+    
+    ~Player(){
+        
+    }
+};
+
+int main(int argc, char *argv[])
+{
+	Player player;
+	player.change_strategy<Defensive_Strategy>();
+	player.change_strategy<Offensive_Strategy>();
+	
+}   
