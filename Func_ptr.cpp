fcf0e85f2e10631d5b383561e413baff59665456
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2025-03-20 11:52:16 -0700

    Update Func_ptr.cpp

--- Func_ptr.cpp
+++ Func_ptr.cpp
@@ -1,17 +1,17 @@
 #include<iostream>
 //To illustrate functions pointers, can we allocate memory on the heap to actually execute the functions
 typedef void (*Func_ptr)(const int&);
+
 void Print_value(const int& value){
     std::cout<<value<<std::endl;
     return;
 }
+
 
 int main(int argc, char *argv[])
 {
 	Func_ptr* func=new Func_ptr;
 	*func=Print_value;
  static_cast<Func_ptr>(*func)(15);
-	
-
 	
 }
