b729bb6d9da4f7c7cf0250df83ea95f52804c5e0
Priesthud <163196772+Priesthoo@users.noreply.github.com>
2025-05-02 16:25:49 +0100

    Update Event system.hpp

--- Event system.hpp
+++ Event system.hpp
@@ -1,4 +1,4 @@
- #include<iostream>
+#include<iostream>
 #include<string>
 #include<list>
 #include<memory>
@@ -69,6 +69,10 @@ class Event:public Object{
         return;
     }
    void SetOrigin_Object(Object* ob){
+       if(ob==nullptr){
+           return;
+       }
+       
        orig_object=ob;
        return;
    }
@@ -78,7 +82,7 @@ class Event:public Object{
     long GetId() const{
         return id;
     }
-    Object* GetOrigin_Object() const{
+    Object* GetOrigin_Object() const{ // when comparing,we will compare addresses .
         return orig_object;
     }
     Event(const Event& event){
@@ -95,80 +99,156 @@ class Event:public Object{
     }
 };
 //This stores information about the event types and an  event.A single event can produce different Event type....
+class CustomEvent:public Event{
+    public:
+    CustomEvent(EventType type,long id):Event(type,id){
+        SetClassName(string("Custom Event"));
+    }
+};
 
-
- template<class EventArg,class Class>
- using Classmethod=void(Class::*)(EventArg& arg);
+ template<class Class>
+ using Classmethod=void(Class::*)(Event& arg);
  
 //This maps the Eventtype, Event and the handler together..,Note EventArg represents the event, EventTag represents the type of event.
-template<class EventArg,class Class>
+template<,class Class>
 class Event_Functor_Mapper{
     
     public:
-    Pair<EventArg,Classmethod<EventArg,Class>> eventfunctormapper;
-    Event_Functor_Mapper(const EventArg& event,Classmethod<EventArg,Class> method){
-        eventfunctormapper.first_pair=event;
-        eventfunctormapper.second_pair=method;
+    Event event;
+    Classmethod<Class>Method;
+    Event_Functor_Mapper(){
+        
+    }
+    Event_Functor_Mapper(const Event& p_event,Classmethod<Class> method){
+        event=p_event;
+        Method=method;
         
     }
     Event_Functor_Mapper(const Event_Functor_Mapper& mapper){
-       eventfunctormapper.first_pair=mapper.first_pair;
-       eventfunctormapper.second_pair=mapper.second_pair;
-       
+       this->event=mapper.event;
+       this->Method=mapper.Method;
     }
     
 };
+template<class Class>  //Forward Declaration
+class EventHandler;
 
-//This is an event handler, It process all the events and execute it's respective handler, It also binds the callback function to it's event
-template<class EventArg,class Class>
-class EventHandler{
-    std::unique_ptr<Event_Functor_Mapper<EventArg,Class>>Handler;
-    long id;  //This stores the id of the event 
-    std::unique_ptr<EventHandler<EventArg,Class>> NextHandler; //The Next Handler will hold the Next handler....
+template<class Class> //Event handler list....
+using Event_Handler_List=list<EventHandler<Class>>;
+/*
+Event Handler will handle events, it Maps Event to member function pointers
+Criteria to convert from one member function pointer to another function pointer...
+1).A derived class must derive from a base class. By that, We can convert from  derived member function pointer to Base memeber function pointer and vice versa....
+
+
+
+*/
+namespace Function{
+    /*
+   This is used when a class provides it's callback function and but it is another class that provides the Event_Handler_List....
     
-    public:
-    EventHandler(){
-        
-    }
- //For every event added, There is also a parent handler,
- /*
- Event handler 
- */
- template<class EventTag,class EvtHandler>
-   void Bind(const EventTag& evttag,Classmethod<EventArg,Class> method,EvtHandler*handler,long id){
+    
+    */
+    template<class EventTag,class Class, class Another_Class,class OriginObject>
+    void Bind(const EventTag& evttag,Classmethod<Another_Class> method_1,Event_Handler_List<Class>* handler,const long& id,OriginObject* object){
         if(handler==nullptr){
-            handler=new EventHandler<EventArg,Class>();
-        
-        if(handler->Handler.get()==nullptr){
-            handler->Handler.reset(new Event_Functor_Mapper<EventArg,Class>);
+            std::cout<<"Handler is empty,To solve this,Either use a custom allocator or alocate from the heap directly,or use smart pointers"<<std::endl;
+            return;
         }
-        //Event Handler 
-      handler->Handler->eventfunctormapper.first_pair=Event(evttag,id);
-      handler->Handler->eventfunctormapper.second_pair=method;
-      handler->id=0;
-      return;
+        EventHandler<Class> evthandler;
+        Event event1(evttag,id);
+        if(evthandler.Handler.get()==nullptr){
+            evthandler.Handler.reset(new Event_Functor_Mapper<Class>);
         }
-  
-  EventHandler<EventArg,Class>::Bind<EventTag,EvtHandler>(evttag,method,handler->NextHandler.get(),id);
+        evthandler.Handler->event=event1;
+        event.SetOrigin_Object(object);
+        evthandler.Handler->Method=static_cast<ClassMethod<Class>>(method_1);
+        handler->push_back(evthandler);
+        return;
+       }
+  /*
+  This is used when a class provides both it's callback function and Event_Handler_List.........
+   
+  */
+   template<class EventTag,class Class,class OriginObject>
+   void Bind_Method(const EventTag& evttag, Classmethod<Class> method,Event_Handler_List<Class>*handler,const long& id,Origin_Object* object){
+       if(handler==nullptr){
+           std::cout<<"Handler is empty,To solve this,Either use a custom allocator or alocate from the heap directly,or use smart pointers"<<std::endl;
+            return;
+        }
+       EventHandler<Class> evthandler;
+       Event event1(evttag,id);
+       if(evthandler.Handler.get()==nullptr){
+            evthandler.Handler.reset(new Event_Functor_Mapper<Class>);
+        }
+        evthandler.Handler->event=event1;
+        event.SetOrigin_Object(object);
+        evthandler.Handler->Method=method;
+        handler->push_back(evthandler);
+        return;
+   }
+   //This is used whrn we have class for event handling and another class for Event_handlet_list....
+  template<class EventTag,class Class,class Another_Class,class Origin_Object>
+  void Bind_Method(const EventTag& evttag,Classmethod<Class> method,Event_Handler_List<Another_Class>*handler,Origin_Object* object){
+       if(handler==nullptr){
+           std::cout<<"Handler is empty,To solve this,Either use a custom allocator or alocate from the heap directly,or use smart pointers"<<std::endl;
+            return;
+        }
+        EventHandler<Another_Class>
   }
-  
-   template<class EventTag,class EvtHandler>
-   static void Unbind(){
-        
-        
+   
+       
+ //This unbinds the Event and the call back function(class Method) from the event handler
+bool Unbind();
+
+   template<class Class,class Another_Class>
+   void ProcessEvent(Event_Handler_List<Class>* handler,Event& event_arg){
+       if(handler==nullptr){
+           std::cout<<"Event_Handler_List  is empty, We can not process any event, Attach an Event_Handler_List to the pointer variable"
+       }
+       for(const auto& Evthandler:*handler){
+           Event arg=Evthandler.Handler->event;
+           if(arg.GetClassName()==event_arg.GetClassName() &&(arg.GetOrigin_Object()==event_arg.GetOrigin_Object())&& (arg.GetType()==event_arg.GetType())&& (arg.GetId()==event_arg.GetId())){
+                Classmethod<Another_Class>  method=static_cast<Classmethod<Another_Class>(EvtHandler.Handler->Method);
+                class Another_Class instance;
+                (instance.*method)(event);
+     }
+       }
+       return;
+   }
+ 
+}
+
+template<class Class>
+class EventHandler:public Object{
+    public:
+    unique_ptr<Event_Functor_Mapper<Class>>Handler;
+    long id;
+    EventHandler(){
+        Handler.reset(new Event_Functor_Mapper<Class>);
+    }
+    EventHandler(unique_ptr<Event_Functor_Mapper<Class>>p_handler,const long& p_id){
+        Handler=std::move(p_handler);
+        id=p_id;
+    }
+    EventHandler(const EventHandler& handler){
+        Handler=std::move(handler.Handler);
+        id=handler.id;
+    }
+    void SetId(const long& p_id){
+        id=p_id;
         return;
     }
-    //EventArg:This type is an Event...
- void ProcessEvent(const EventArg& event_arg){
-     
-      if(EventArg.GetClassName()==string("Event")){
-          Event_Handler<EventArg,Class>* handler_iter;
-     while(handler_iter!=nullptr){
-     
-     }
-      }
-      //Events are propagated to the current handler, if the parent_handler is even specified...
-     
-}      
-  
+    void SetHandler(unique_ptr<Event_Functor_Mapper<Class> ptr){
+        Handler=std::move(ptr);
+        return;
+    }
+template<class EventTag,class AnotherClass,class OriginObject>
+void Bind(const EventTag& evttag, Classmethod<AnotherClass>method_1,Event_Handler_List<Class>* handler,OriginObject* obj){
+     Function::Bind<EventTag,Class,AnotherClass,OriginObject>(evttag,method_1,handler,obj);
+}
+template<class EventTag,class OriginObject>
+void Bind(const EventTag& evttag,Classmethod<Class>method,Event_handler_List<Class>* handler,OriginObject* object);
+    Function::Bind_Method(evvtag,method,handler,object);
+
 };
